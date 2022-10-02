68c5e0c4db8cbb49e28f651c9ca69d3dd34aaee3
Gershon <gershon@pop-os.localdomain>
2022-10-02 21:03:37 +0300

    oct2-2022

---
+++ oct2-prob1.js
@@ -0,0 +1,6 @@
+function fixTheMeerkat(arr){
+    arr.reverse()
+    return arr
+    //we reverse it so that whatever input is reversed since the tests and attempt succeeded.
+    //clearly was meant to introduce/test for .reverse() method
+}
