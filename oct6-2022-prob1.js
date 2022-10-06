8860ec2e62cfaa31e718331fe77a07536d69e4be
Gershon <gershon@pop-os.localdomain>
2022-10-06 22:21:22 +0300

    last 3 days of Kippur/after

---
+++ oct6-2022-prob1.js
@@ -0,0 +1,7 @@
+//even or odd
+//create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers
+
+function even_or_odd(number) {
+    if(number % 2 === 0){return "Even"}else{
+  return "Odd"};
+  }
